1e987207fea4b4446cbd7b9a5cf34540f67a40d5
Michael Fenwick <mickyfen17@aol.com>
2017-10-03 23:32:32 -0600

    complete logo

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,6 +1,13 @@
 import React, { Component } from 'react';
 import glamorous from 'glamorous';
 import Logo from './Logo';
+import { css } from 'glamor';
+
+const Scale = css.keyframes({
+  '0%': { transform: 'scale(1)' },
+  '50%': { transform: 'scale(1.1)' },
+  '100%': { transform: 'scale(1)' },
+});
 
 const Wrapper = glamorous.section({
   display: 'flex',
@@ -14,11 +21,12 @@ const Circle = glamorous.div({
   display: 'flex',
   justifyContent: 'center',
   alignItems: 'center',
-  height: '600px',
+  height: '570px',
   width: '600px',
   background: '#282c34',
   borderRadius: '50%',
   border: '10px solid #757575',
+  animation: `${Scale} 5s infinite ease-out`,
 });
 
 class App extends Component {
--- src/Components/Logo.js
+++ src/Components/Logo.js
@@ -1,5 +1,11 @@
 import React, { Component } from 'react';
 import glamorous from 'glamorous';
+import { css } from 'glamor';
+
+const colorTransition = css.keyframes({
+  '0%': { borderColor: '#2ca9dc' },
+  '100%': { borderColor: '#61dafb' },
+});
 
 const CenterBall = glamorous.div({
   height: '72px',
@@ -7,11 +13,32 @@ const CenterBall = glamorous.div({
   background: '#61dafb',
   borderRadius: '50%',
   borderBottom: '8px solid #2ca9dc',
+  transform: 'none',
 });
+
+const HorzOval = glamorous.div({
+  animation: `${colorTransition} 5s infinite`,
+  position: 'absolute',
+  width: '435px',
+  height: '125px',
+  borderRadius: '50%',
+  border: 'solid 18px #61dafb',
+});
+
+const VertOval = glamorous(HorzOval)(props => ({
+  width: '460px',
+  height: '150px',
+  transform: props.left ? 'rotate(55deg)' : 'rotate(-55deg)',
+}));
 
 class Logo extends Component {
   render() {
-    return <CenterBall />;
+    return [
+      <CenterBall key={0} />,
+      <HorzOval key={1} />,
+      <VertOval key={2} left />,
+      <VertOval key={3} right />,
+    ];
   }
 }
 
